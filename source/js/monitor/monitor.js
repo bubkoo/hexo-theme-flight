1e29c4700793a888b78404b9a51f64198a047076
bubkoo <bubkoo@163.com>
2014-03-04 21:59:51 +0800

    sea

--- source/js/monitor/monitor.js
+++ source/js/monitor/monitor.js
@@ -2,6 +2,7 @@
  * Created by Johnny.Peng on 14-3-4.
  */
 define(function (require, exports, module) {
-
+    var detector = require("detector");
+    var Events = require("events");
 
 });
---
+++ source/js/sea.config.js
@@ -0,0 +1,9 @@
+seajs.config({
+    'base' : '/js/',
+    'alias': {
+        '$': '/lib/jquery-1.10.1.min',
+        'events': '/monitor/events',
+        'aspect': '/monitor/aspect'
+    },
+    'charset': 'utf-8'
+});

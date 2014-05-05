1bb328bf38da4c1925968d58cca2c9edc99e510a
bubkoo <bubkoo@163.com>
2014-05-05 14:54:56 +0800

    bigfix: rename some variable

--- source/js/jquery.scrollspy.js
+++ source/js/jquery.scrollspy.js
@@ -1,23 +1,27 @@
 ;
 (function(window, $, undefined) {
+
 	$.fn.extend({
 		scrollspy: function(options) {
+
 			var defaults = {
 				min: 0,
 				max: 0,
 				vertical: true,
 				offset: 0,
 				container: window,
-				onEnter: options.onEnter ? options.onEnter : [],
-				onLeave: options.onLeave ? options.onLeave : [],
-				onTick: options.onTick ? options.onTick : []
+				onEnter: options.onEnter ? options.onEnter : function() {},
+				onLeave: options.onLeave ? options.onLeave : function() {},
+				onTick: options.onTick ? options.onTick : function() {}
 			};
 
 			var options = $.extend({}, defaults, options);
 
-            return this.each(function (i) {
-                var element = this;
-            });
+			return this.each(function(i) {
+				var element = this;
+
+			});
 		}
 	});
+
 })(window, jQuery);
